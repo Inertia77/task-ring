4dbd40c5666150bc71948a6a41296364d91406bb
Inertia <153355615+Inertia77@users.noreply.github.com>
2026-09-22 00:06:57 +0900

    fix(gamequest): sync final tier v3 service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -46,7 +46,6 @@ const APP_SHELL = [
   "./assets/js/views/product-ui.js",
   "./assets/js/game-disclosure.js",
   "./assets/js/game-tier-system.js",
-  "./assets/js/game-tier-editor.js",
   "./assets/js/ux-efficiency.js",
   "./assets/js/private-restructure.js",
   "./assets/js/pwa.js"
